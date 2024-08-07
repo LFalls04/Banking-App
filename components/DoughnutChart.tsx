e1c84a7554f396af6ec0252c91a34bf34b2183e3
Lathan Falls <lathanfalls04@gmail.com>
2024-08-07 02:23:59 -0500

    progressing project

---
+++ components/DoughnutChart.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend }
+from "chart.js";
+import { Doughnut } from "react-chartjs-2"
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+
+const DoughnutChart = ({ accounts }: ) => {
+  return <Doughnut data={[]} />
+}
+
+export default DoughnutChart;
